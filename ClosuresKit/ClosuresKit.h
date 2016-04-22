7619ade10e486e3249fe4d49b5b2ad7b684511e5
卓同学 <lacklock@gmail.com>
2016-04-22 17:04:27 +0800

    first init

---
+++ ClosuresKit/ClosuresKit.h
@@ -0,0 +1,19 @@
+//
+//  ClosuresKit.h
+//  ClosuresKit
+//
+//  Created by 卓同学 on 16/4/22.
+//  Copyright © 2016年 zhuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ClosuresKit.
+FOUNDATION_EXPORT double ClosuresKitVersionNumber;
+
+//! Project version string for ClosuresKit.
+FOUNDATION_EXPORT const unsigned char ClosuresKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ClosuresKit/PublicHeader.h>
+
+
